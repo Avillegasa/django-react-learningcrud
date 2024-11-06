5fd58c3c60601440556a40433e0843f83f27cf40
Avillegasa <andres.villegasa19@gmail.com>
2024-11-05 23:59:16 -0400

    modelado de tareas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Aca creamos nuestras tablas que posteriormente seran generadas en db.sqlite3
+# Despues de realizar el class respectivo, realizar: 
+# python manage.py makemigrations --> Crea el codigo para ejecutar y crear la tabla
+# python manage.py migrate --> Ejecuta el codigo y la tabla ya es creada en db.sqlite3
+
+
+class Task(models.Model): 
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title

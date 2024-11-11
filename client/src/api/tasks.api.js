248249b31d4a20342e259aeb7a4ff1df53bc0a39
Avillegasa <andres.villegasa19@gmail.com>
2024-11-10 23:33:42 -0400

    Delete task

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -8,3 +8,5 @@ const tasksApi = axios.create({
 export const getAllTasks = () => tasksApi.get('/');
 
 export const createTask = (task) => tasksApi.post('/', task);
+
+export const deleteTask = (id) => tasksApi.delete(`/${id}/`)
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form'
-import {createTask} from '../api/tasks.api'
-import {useNavigate} from 'react-router-dom'
+import {createTask, deleteTask} from '../api/tasks.api'
+import {useNavigate, useParams} from 'react-router-dom'
 
 
 export function TaskFormPage() {
@@ -11,6 +11,8 @@ export function TaskFormPage() {
         formState: { errors } 
     } = useForm();
     const navigate = useNavigate()
+    const params = useParams()
+    console.log(params)
 
     const onSubmit = handleSubmit(async data => {
         await createTask(data);
@@ -35,6 +37,16 @@ export function TaskFormPage() {
             
             <button>Save</button>
         </form>
+
+
+            {params.id && <button onClick={async () => {
+                const accepted = window.confirm('Are you sure?')
+                if (accepted) {
+                    await deleteTask(params.id);
+                    navigate('/tasks');
+                }
+            }}>Delete</button>}
+
         </div>
     )
-};
+}
